9e211aea40451ac4cf4f33d61c8ae6ef062e5455
AnabelaSilva <up201506034@fe.up.pt>
2020-05-31 11:08:51 +0100

    First Chart

--- public/javascript/charts.js
+++ public/javascript/charts.js
@@ -1,23 +1,23 @@
 
 window.onload = function () {
-
+    let  color = ["red", "orange", "yellow", "green", "blue", "purple", "grey"];
+    let colorIndex = 0;
     let datasets = [];
     labels = [];
     results.forEach(element => {
         console.log(element.name);
-        let set = datasets.find(el => {return el.label == element.name;});
-        let label = labels.find(el => {return el == element.WeekNumber;});
-        if(label == null){
-            label =  element.WeekNumber;
+        let set = datasets.find(el => { return el.label == element.name; });
+        let label = labels.find(el => { return el == element.WeekNumber; });
+        if (label == null) {
+            label = element.WeekNumber;
             labels.push(label);
         }
-        if(set == null){
-            set = { label: element.name, data:[]};
+        if (set == null) {
+            set = { label: element.name, data: [], fill: false, borderColor: color[colorIndex++] };
             datasets.push(set);
         }
-        set.data[element.WeekNumber]= element.Visitors;
+        set.data[element.WeekNumber] = element.Visitors;
     });
-    console.log(labels);
     var ctx = document.getElementById('myChart').getContext('2d');
     var mixedChart = new Chart(ctx, {
         type: 'line',
@@ -32,7 +32,7 @@ window.onload = function () {
             title: {
                 display: true,
                 text: 'Activity by course'
-            },
-            
-    }});
+            }
+        }
+    });
 }
