30b41daa36f13a9f65b9a56bfc744a2cf6d07fa0
AnabelaSilva <up201506034@fe.up.pt>
2020-06-03 10:43:58 +0100

    Comecar a implementar prototipo

--- QUERIES TRAININ.sql
+++
@@ -1,5 +0,0 @@
-SELECT CAST(((julianday(date) - julianday('2020-02-10 00:00:00'))/7) AS Integer) AS WeekNumber, count(student) AS Visitors, name, NumStudents
-FROM Activity 
-JOIN Course ON(Course.id = Activity.course)
-JOIN (SELECT count(student) AS NumStudents, course FROM Student_in_Course Group by course)AS tab_1 ON(Course.id = tab_1.course)
-GROUP BY Activity.course, WeekNumber;
--- server.js
+++ server.js
@@ -11,13 +11,7 @@ app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-  res.render('index', {
-    title: 'Homepage'
-  });
-});
-
-app.get('/grades', (req, res) => {
-  let sql = "SELECT CAST(((julianday(date) - julianday('2020-02-10 00:00:00'))/7) AS Integer) AS WeekNumber, count(student) AS Visitors, name, NumStudents FROM Activity JOIN Course ON(Course.id = Activity.course) JOIN (SELECT count(student) AS NumStudents, course FROM Student_in_Course Group by course)AS tab_1 ON(Course.id = tab_1.course)  GROUP BY Activity.course, WeekNumber;";
+  let sql = "SELECT CAST(((julianday(date) - julianday('2020-03-10T00:00:00'))/7) AS Integer) AS WeekNumber, name, 1.0*count(date)/NumStudents AS VisitByStudent FROM Activity JOIN Course ON(Course.id = Activity.course)  JOIN (SELECT count(student) AS NumStudents, course FROM Student_in_Course Group by course) AS tab_1 ON(Course.id = tab_1.course) GROUP BY Activity.course, WeekNumber;";
   let params = [];
   db.all(sql, params,
     function (err, rows) {
@@ -27,19 +21,12 @@ app.get('/grades', (req, res) => {
         return err;
       }
       let activity_by_course = rows;
-      sql = "SELECT DaysSinceActivity, count(id) AS NumberOfStudents FROM(SELECT CAST(julianday('now') - julianday(max(date)) AS Integer) AS DaysSinceActivity, id FROM Student LEFT JOIN Activity ON (Student.id = Activity.student) GROUP BY student)GROUP By DaysSinceActivity;";
-      db.all(sql, params,
-        function (err, rows) {
-          if (err) {
-            console.error(err);
-            console.trace();
-            return err;
-          }
-          console.log(rows);
-          res.render('show', { java: 'charts', data: activity_by_course, pieResults: rows});
-        });
+      res.render('index', { title: 'Homepage', data: activity_by_course });
     }
   );
+});
+
+app.get('/grades', (req, res) => {
 });
 app.get('/student', (req, res) => {
   let sql = "SELECT Student.name AS StudentName, Course.name AS CourseName, Evaluation.name AS EvaluationName, value FROM Student JOIN Student_in_Course ON (Student.id = Student_in_Course.student)  JOIN Grade ON (Student.id = Grade.student)  JOIN Evaluation ON (Grade.evaluation = Evaluation.id AND Evaluation.course = Student_in_Course.course)  JOIN Course ON (Course.id = Student_in_Course.course) WHERE Student.id = ? ORDER BY Course.id;";
